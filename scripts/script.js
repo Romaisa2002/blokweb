a9fb608c4cef4cfd3783040a381d94337a241111
Romaisa2002 <romaisa.zaman@hva.nl>
2025-11-27 09:30:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+const OpenMenuButton = document.querySelector("header > button");
+const SluitMenuButton = document.querySelector("header nav button");
+const deNav = document.querySelector("header > nav");
+
+OpenMenuButton.onclick = OpenMenu;
+SluitMenuButton.onclick = SluitMenu;
+
+function OpenMenu(){
+  deNav.classList.add("is-open");
+}
+
+function SluitMenu(){
+  deNav.classList.remove("is-open");
+}
+
